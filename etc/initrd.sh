f62eb457455cea3113c04ebadbf06bdfe5288d61
Micah Scott <micah@scanlime.org>
2021-11-21 21:24:46 -0800

    working on initrd init script

--- etc/initrd.sh
+++ etc/initrd.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+while true; do
+        (
+                set -ve
 
-echo hello from initrd
+                mount -t proc none /proc
+                mount -t sysfs none /sys
+                mkdir -p /new_root
+
+                /lib/udev/pcmcia-socket-startup 0
+                /lib/udev/pcmcia-socket-startup 1
+
+                mount /dev/sdb1 /new_root
+        )
+        if [ $? -eq 0 ]; then
+                break
+        else
+                echo initrd failed, dropping to local shell. Exit to retry.
+                /bin/sh
+        fi
+done
+
+exec /bin/busybox switch_root /new_root /sbin/init
 
